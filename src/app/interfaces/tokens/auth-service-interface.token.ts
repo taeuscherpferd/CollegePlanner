0fcc8edfdffa5d032bf86eb04624deddd9b37dfe
Kailean O'Keefe <ibber@myokcorral.com>
2020-02-01 14:31:11 -0700

    Implemented the MainViewComponent + outlining

---
+++ src/app/interfaces/tokens/auth-service-interface.token.ts
@@ -0,0 +1,3 @@
+import {InjectionToken} from '@angular/core';
+
+export const AUTH_SERVICE = new InjectionToken<string>('authService');
---
+++ src/app/services/settings.service.ts
@@ -0,0 +1,5 @@
+import { ISettingsService } from '../interfaces/services/settings-service.interface';
+
+export class SettingsService implements ISettingsService  {
+
+}
